bf1603c0b03fec50af3153fef564093552d9a57e
Dhirendra Singh <dhi.singh@gmail.com>
2021-06-15 11:00:26 +1000

    Updated SCS scenario

--- plan-algorithm/SCS-Locations-2021/SCS-Locations-2021-to-csv.sh
+++ plan-algorithm/SCS-Locations-2021/SCS-Locations-2021-to-csv.sh
@@ -5,6 +5,12 @@ shp=$dir/SCS-Locations-2021.shp
 outcsv=$dir/SCS-Locations-2021.csv
 echo "Extracting attributes table from $shp to $outcsv"
 ogr2ogr -f "CSV" $outcsv $shp
+
+# adjust some values (should be adjusted in input instead)
+# sed -i -e 's/,Apollo Bay Region,"1000"/,Apollo Bay Region,"10"/' $outcsv
+# sed -i -e 's/,West Region,"100"/,West Region,"10"/' $outcsv
+# sed -i -e 's/,North West Region,"100"/,North West Region,"10"/' $outcsv
+
 # ogrinfo -al $shp > $shpdir/xy.ogrinfo
 # awk -F"[()]" '/POINT/{print $2}' $shpdir/xy.ogrinfo | \
 #   gdaltransform -s_srs EPSG:4326 -t_srs EPSG:32754 -output_xy | \
